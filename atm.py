d743dc41970d33af37d71bd74d1d7ced164081ab
Uday Punmiya <38768570+upunmiy1@users.noreply.github.com>
2019-03-28 14:53:26 -0400

    Update atm.py
    Made some changes on line no. 27

--- atm.py
+++ atm.py
@@ -1,4 +1,4 @@
-#Intitialization Code
+# Intitialization Code
 Pin = 1234        
 Starting_balance = 10000
 twenty = 0
@@ -10,7 +10,7 @@ Pin_entry = int(input("Enter the PIN: "))  #Takes the input from user for pin
 if Pin_entry == Pin:
     print("Correct:\n")
     
-    Withdraw = int(input("Enter the amount to withdraw: ")) #takes the input from user for money to witghdraw
+    Withdraw = int(input("Enter the amount to withdraw:")) #takes the input from user for money to witghdraw
     
     if Withdraw <= Starting_balance:
         twenty = int(Withdraw/20)
@@ -24,10 +24,9 @@ if Pin_entry == Pin:
         
         
     else:
-        print("Invalid Entry, Do you want to proceed?\n")
-        K = str(input())                #takes input from user inorder to move ahead
+        K = str(input("Current entry is more than the balance in atm, Do you want to proceed, You might be charged?\n (YES/NO): "))                #takes input from user inorder to move ahead
         
-        if K == 'YES':
+        if(K == 'YES' or K=='yes'):
             print("You have been charged with penalty for: 20 $")
         else:
             print("Returning to the main Window.")
